acf394cda3cda607fd1b0b631d5e222f76721f54
Harsh Dhaduk <cmp.eng.harsh@gmail.com>
2022-07-18 18:08:55 +0530

    Add ebilling cusotmer api.

--- wb_ebilling_integration/models/ebilling_registration.py
+++ wb_ebilling_integration/models/ebilling_registration.py
@@ -140,7 +140,7 @@ class WBRequestRegistration(models.Model):
         return True, "Registered successfully."
 
     def getCustomerList(self):
-        return [{'name':prd.display_name, 'id':prd.id} for prd in self.env['res.partner'].search([('id','>',5)])]
+        return [{'name':prd.display_name, 'id':prd.id} for prd in self.env['res.partner'].sudo().search([('id','>',5)])]
 
     def getProductList(self):
         return [{'id':prd.id, 'name':prd.name} for prd in
